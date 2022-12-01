2bdc3b49ff7ada850be529dd81e2d420fad98933
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-12-01 12:09:02 -0800

    refactored 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-def arguments():
-    arg_string = "arguments"
-    c_or_d = "."
-    length = len(argv) - 1
-    if (length == 1):
-        arg_string = "argument"
-    if (length - 1 >= 0):
-        c_or_d = ":"
-
-    print("{} {}{}".format(length, arg_string, c_or_d,))
-
-    for index, arg in enumerate(argv):
-        if (index > 0):
-            print("{}: {}".format(index, arg))
-    if __name__ == "__main__":
-        arguments()
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
