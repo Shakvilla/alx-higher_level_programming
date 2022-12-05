d8e0c9363c5c7eb7f0bab4bd773d91b80c1fc29b
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-12-05 12:03:06 -0800

    refactored code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,7 +7,8 @@ def no_c(my_string):
     new_string = my_string[:]
 
     for i in range(length):
-        if my_string[i] == 'c' or my_string[i] == 'c':
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
             new_string = new_string[:(i - j)] + my_string[(i + 1):]
             j += 1
-    return new_string
+
+    return (new_string)
