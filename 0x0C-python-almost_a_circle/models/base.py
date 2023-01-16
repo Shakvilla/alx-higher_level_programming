a0bc70f9fd3766af67888936a80c8270243bbcd7
Shakvilla <abdulshakuraclement@yahoo.com>
2023-01-16 19:39:18 +0000

    a folder named models with an empty file __init__.py inside - with this file, the folder will become a Python package

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+
+class Base:
+    """Represent the base model.
+
+    Represents the "base" for all other classes in this task.
+
+    Attributes:
+    __nb_objects (int): The number of instantiated Base.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        
