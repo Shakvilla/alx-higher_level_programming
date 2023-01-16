6781ff945293353ad19b84aa3dd178b69d2f8f6a
Shakvilla <abdulshakuraclement@yahoo.com>
2023-01-16 21:12:55 +0000

    fixed issue

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+class Base:
+    """Represents the "base" for all other classes in the project
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
