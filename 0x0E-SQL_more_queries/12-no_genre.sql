d20d09b56a67ad7d1f9a77d0a395e86e6c6e4568
Shakvilla <abdulshakuraclement@yahoo.com>
2023-10-19 10:53:47 +0000

    done with all tasks

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-LEFT JOIN tv_shows
+RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ;
